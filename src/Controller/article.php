8f6c3ca412be985defc60cf2c278346ee3b4e5ef
gino <ginovito@live.fr>
2019-02-07 15:31:55 +0100

    add users entity

--- src/Controller/article.php
+++ src/Controller/article.php
@@ -1,20 +1,28 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Service\SwearCleaner;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class article
 {
      /**
-     * @Route("/article", requirements={"id"="\d+"})
+     * @Route("/article/{id}", name="singleView", requirements={"id"="\d+"})
      */
-    public function articleView()
+    public function single($id = 1, SwearCleaner $swearCleaner)
     {
-        $number = "Hello World, it's an single";
-        return new Response(
-            '<html><body> '.$number.'</body></html>'
-        );
+       $repository = $this->getDoctrine()->getRepository(Article::class);
+       $article = $repository->find($id);
+       if (!$article) {
+        throw $this->createNotFoundException("L'article que vous cherchez n'existe pas");
+       }
+       $article = $swearCleaner->cleanSwear($article);
+       return $this->render("article/single.html.twig", ["article" => $article]);
     }
 
 }
